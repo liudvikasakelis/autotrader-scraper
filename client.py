6e67e719ac9b954c9339fe3986abd6e5871be61d
Šarūnas Ažna <sarunas.azna@gmail.com>
2017-03-12 19:51:44 +0200

    pep8

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import socket
+
+
 def client(HOST, PORT, message):
-    if(not message):
-        raise ValueError('message of length 0')    
+    if not message:
+        raise ValueError('message of length 0')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
     sock.sendall(str(message))
@@ -10,9 +12,19 @@ def client(HOST, PORT, message):
     sock.shutdown(0)
     sock.close()
 
+
 def add_query(postcode, radius, price_from, price_to):
-    url = 'http://www.autotrader.co.uk/car-search?postcode=' + str(postcode) + '&radius=' + str(radius) + '&price-from=' + str(price_from) + '&price-to=' + str(price_to) + '&page='
+    url = (
+        'http://www.autotrader.co.uk/car-search?postcode={postcode}&radius='
+        '{radius}&price-from={price_from}&price-to={price_to}&page='.format(
+            postcode=postcode,
+            radius=radius,
+            price_from=price_from,
+            price_to=price_to
+        )
+    )
     client('localhost', 9500, 'c' + url)
+
 
 def shutdown():
     client('localhost', 9500, 'shutdown')
--- srv.py
+++ srv.py
@@ -10,6 +10,7 @@ from lxml import html
 import ff1
 import sqlite3
 
+
 def add_jlist(job):
     global jlock
     global jlist
@@ -20,6 +21,7 @@ def add_jlist(job):
     jlock = False
     return 0
 
+
 def add_rlist(result):
     global rlist
     global rlock
@@ -29,32 +31,37 @@ def add_rlist(result):
     rlist.append(result)
     rlock = False
     return 0
-      
+
+
 def do_c(job):
-    page = requests.get(job[1]+"1") 
-    if(str(page)=='<Response [204]>'): # Network stuff
-        sleep(120)        
+    page = requests.get(job[1] + "1")
+    if str(page) == '<Response [204]>':  # Network stuff
+        sleep(120)
         return 1
-    
-    t = html.fromstring(page.content) 
-    v = t.xpath('//div[@class="search-result__r1"]/div/a/@href') 
-    t_cars = t.xpath('//h1[@class="search-form__count js-results-count"]/text()')[0]
+
+    t = html.fromstring(page.content)
+    t.xpath('//div[@class="search-result__r1"]/div/a/@href')
+    t_cars = t.xpath(
+        '//h1[@class="search-form__count js-results-count"]/text()'
+    )[0]
     t_cars = re.search('[0-9,]+', t_cars).group(0)
-    t_cars = int(t_cars.replace(",", "")) 
-    pageno = t_cars/10 + 1
+    t_cars = int(t_cars.replace(",", ""))
+    pageno = t_cars / 10 + 1
 
     for i in range(pageno):
-        add_jlist(("b", job[1] + str(i+1))) # c jobs produce b jobs
+        add_jlist(("b", job[1] + str(i + 1)))  # c jobs produce b jobs
 
     print("C " + str(pageno))
     return 0
 
+
 def do_b(job):
     count = 0
     page = requests.get(job[1])
-    
-    if(str(page)=='<Response [204]>'): # Network stuff
-        sleep(120)        
+
+    # FIXME: page.status == 204
+    if(str(page) == '<Response [204]>'):  # Network stuff
+        sleep(120)
         return 1
 
     t = html.fromstring(page.content)
@@ -62,59 +69,61 @@ def do_b(job):
 
     for a in v:
         ID = re.search('(?<=classified/advert/)[0-9]+', a)
-        if(ID):
+        if ID:
             ID = int(ID.group(0))
-            add_jlist(("a", ID)) # b jobs produce a jobs
-            count += 1    
+            add_jlist(("a", ID))  # b jobs produce a jobs
+            count += 1
 
     print("B " + str(count))
     return 0
 
+
 def do_a(job):
     global rlock
-    global rlist    
+    global rlist
     result = ff1.scraper(job[1])
 
-    if(result[1] == 'AUTOTRADER_FAILED'):
+    if result[1] == 'AUTOTRADER_FAILED':
         print("Server failed")
         sleep(60)
-        return(1)
+        return 1
 
     while rlock:
         sleep(random.uniform(0.01, 0.1))
-    rlock = True 
+    rlock = True
     rlist.append(result)
     rlock = False
     print("A")
     return 0
 
+
 def sql_writer():
     global STAHP
     global rlist
     global rlock
-    with sqlite3.connect('test.db') as conn:    
+    with sqlite3.connect('test.db') as conn:
         c = conn.cursor()
         c.execute('CREATE TABLE IF NOT EXISTS cars ('
-        'url INTEGER PRIMARY KEY,'
-        ' make TEXT,'
-        ' model TEXT,'
-        ' engine_size NUM,'
-        ' year INT,'
-        ' body_type TEXT,'
-        ' mileage NUM,'
-        ' transmission TEXT,'
-        ' fuel TEXT,'
-        ' co2_emissions NUM,'
-        ' doors INT,'
-        ' seats INT,'
-        ' category TEXT,'
-        ' engine_power NUM,'
-        ' drive_train TEXT,'
-        ' price NUM,'
-        ' seller TEXT,'
-        ' full_desc TEXT,'
-        ' last_seen NUM,'
-        ' first_gone NUM);')
+                  'url INTEGER PRIMARY KEY,'
+                  ' make TEXT,'
+                  ' model TEXT,'
+                  ' engine_size NUM,'
+                  ' year INT,'
+                  ' body_type TEXT,'
+                  ' mileage NUM,'
+                  ' transmission TEXT,'
+                  ' fuel TEXT,'
+                  ' co2_emissions NUM,'
+                  ' doors INT,'
+                  ' seats INT,'
+                  ' category TEXT,'
+                  ' engine_power NUM,'
+                  ' drive_train TEXT,'
+                  ' price NUM,'
+                  ' seller TEXT,'
+                  ' full_desc TEXT,'
+                  ' last_seen NUM,'
+                  ' first_gone NUM);')
 
         while not STAHP:
             while(rlist):
@@ -126,11 +135,12 @@ def sql_writer():
                 else:
                     final = job
                 c.execute("DELETE FROM cars WHERE url=?", [job[0]])
-                c.execute('INSERT INTO cars VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?'
-                          ',?,?,?,?,?,?)', final)
+                c.execute(
+                    'INSERT INTO cars VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?'
+                    ',?,?,?,?,?,?)', final)
                 while rlock:
                     sleep(random.uniform(0.01, 0.1))
-                rlock = True 
+                rlock = True
                 rlist.remove(job)
                 rlock = False
             conn.commit()
@@ -139,22 +149,24 @@ def sql_writer():
 
 # This combines lines (one line in newline, multiple in olds)
 def combine_lines(olds, newline):
-    final = newline 
+    final = newline
     for old in olds:
         final = combine_two(final, old)
-    return final 
+    return final
+
 
 # The bigger entries are kept for each field
 # except for the last field, in which the smallest non-None entry is kept
 def combine_two(newline, oldline):
     final = newline
-    for i in range(len(final)-1):
-        final[i] = max(final[i], oldline[i])  
+    for i in range(len(final) - 1):
+        final[i] = max(final[i], oldline[i])
     if not final[-1]:
         final[-1] = oldline[-1]
     elif oldline[-1]:
         final[-1] = min(final[-1], oldline[-1])
-    return final 
+    return final
+
 
 def jparse(data):
     if(data[0] == "a"):
@@ -162,7 +174,8 @@ def jparse(data):
         ret[0] = "a"
         try:
             ret[1] = int(data[1:])
-        except:
+        # FIXME: Undefined exception
+        except Exception:
             return 0
         return ret
     elif(data[0] == "b"):
@@ -178,16 +191,17 @@ def jparse(data):
     else:
         return 0
 
+
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
-        global STAHP        
+        global STAHP
         data = self.request.recv(1024)
-        if(data=='shutdown'):
+        if(data == 'shutdown'):
             STAHP = True
             self.request.sendall("shutting down")
             while(self.request.recv(1024)):
-                pass             
+                pass
             return 0
         job = jparse(data)
         if job:
@@ -195,21 +209,22 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
             if(status == 0):
                 self.request.sendall("added")
             else:
-                self.request.sendall("failed to add (%s)"%(status))
+                self.request.sendall("failed to add (%s)" % (status))
         else:
-            self.request.sendall("not a correct command")   
+            self.request.sendall("not a correct command")
         while(self.request.recv(1024)):
-            pass 
+            pass
         print("*")
-        
+
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
 
+
 jlock = False   # Lock to jlist
 jlist = []      # Job list
-rlock = False   # Lock to rlist 
-rlist = []      # Result list 
+rlock = False   # Lock to rlist
+rlist = []      # Result list
 HOST, PORT = "localhost", 9500
 STAHP = False   # How the main loop knows when to STAHP
 
@@ -222,26 +237,26 @@ ip, port = server.server_address
 server_thread = threading.Thread(target=server.serve_forever)
 # Exit the server thread when the main thread terminates
 server_thread.daemon = True
-server_thread.start()  
+server_thread.start()
 
-# This thread to move things from rlist to a sqlite3 database file 
+# This thread to move things from rlist to a sqlite3 database file
 # Always running to avoid starting a new connection for each entry
 sql_thread = threading.Thread(target=sql_writer)
 sql_thread.daemon = True
 sql_thread.start()
 
 # Pay attention, this is the MAIN LOOP:
-while not STAHP:        
+while not STAHP:
     sleep(random.gammavariate(1, 1))
     if(jlist):
         job = random.choice(jlist)
         if(job[0] == "a"):
-            status = do_a(job)          
+            status = do_a(job)
         elif(job[0] == "b"):
             status = do_b(job)
         elif(job[0] == "c"):
             status = do_c(job)
-        
+
         if(status == 0):
             while jlock:
                 sleep(random.uniform(0.01, 0.1))
@@ -251,11 +266,11 @@ while not STAHP:
         else:
             print("job failed:")
             print(job)
-            print("status: "+ str(status))
+            print("status: " + str(status))
 
 # If we have unfinished jobs in jlist, that's pickled for later
 # surely SOMEONE should import them at startup then
-if(jlist):  
+if(jlist):
     with open(str("jlist.txt"), "wb") as fl:
         pickle.dump(jlist, fl)
 
