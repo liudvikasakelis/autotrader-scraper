ed16bedb2753a44374d081b0472bed1081491e72
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-04-15 22:39:28 +0100

    script_template follows the client

--- script_template.py
+++ script_template.py
@@ -6,6 +6,7 @@ sys.path.append(A_PATH)
 import client
 
 PORT = 9500
+HOST = 'localhost'
 postcode = ''
 radius = 0
 price_from = 0
@@ -14,17 +15,19 @@ price_to = 0
 client.multiple_queries(postcode=postcode,
                         radius=radius,
                         price_from=price_from,
-                        price_to=price_to)
+                        price_to=price_to,
+                        HOST=HOST,
+                        PORT=PORT)
 
-while int(client.client('localhost', PORT, 'bc_status')) > 0:
+while int(client.client('bc_status', HOST=HOST, PORT=PORT)) > 0:
     time.sleep(3)
 print('flushing a_list')
-client.client('localhost', PORT, 'a_flush')
+client.client('a_flush', HOST=HOST, PORT=PORT)
 time.sleep(1)
-client.client('localhost', PORT, 'afd')
+client.client('afd', HOST=HOST, PORT=PORT)
 time.sleep(1)
-client.client('localhost', PORT, 'update')
-while int(client.client('localhost', PORT, 'a_status')) > 0:
+client.client('update', HOST=HOST, PORT=PORT)
+while int(client.client('a_status', HOST=HOST, PORT=PORT)) > 0:
     time.sleep(3)
 print('afd and update complete')
-client.client('localhost', PORT, 'shutdown')
+client.client('shutdown', HOST=HOST, PORT=PORT)
